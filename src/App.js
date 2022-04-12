6de09dc50c178f642a269e7fc795777559043d96
Nemanja Vidovic <hefest93@yahoo.com>
2022-04-12 13:01:26 +0200

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+
+import LogIn from "./components/Log-In/LogIn";
+
 function App() {
-  return <div></div>;
+    return (
+        <Routes>
+            <Route path={"/log-in"} element={<LogIn />} />
+        </Routes>
+    );
 }
 
 export default App;
---
+++ src/components/UI/PageWrapper.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PageWrapper = ({ children }) => {
+    return <div className="pageWrapper">{children}</div>;
+};
+
+export default PageWrapper;
