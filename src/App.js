f87a85c94c477395386086feb2d9d7e894496b72
Nemanja Vidovic <hefest93@yahoo.com>
2022-04-13 12:52:06 +0200

    created SignUp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Store from "./components/Store/Store";
 import LogIn from "./components/LogIn-SignUp/LogIn";
+import SignUp from "./components/LogIn-SignUp/SignUp";
 
 function App() {
     return (
         <Routes>
             <Route path="/store" element={<Store />} />
             <Route path="/log-in" element={<LogIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
         </Routes>
     );
 }
