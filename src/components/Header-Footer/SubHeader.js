1feb6d5c37412d591a7db37c3bead6b23050453c
Nemanja Vidovic <hefest93@yahoo.com>
2022-04-16 19:36:26 +0200

    subheader finished

---
+++ src/components/Header-Footer/SubHeader.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { GiHamburgerMenu } from "react-icons/gi";
+import { BsCaretDownFill } from "react-icons/bs";
+
+const SubHeader = () => {
+    return (
+        <div className="subheader">
+            <div className="subheader-inner">
+                <div className="subheader-item center">
+                    <GiHamburgerMenu className="subheader-burger" />
+                    All
+                </div>
+
+                <div className="subheader-item center">Today's Deals</div>
+
+                <div className="subheader-item center">Buy Again</div>
+
+                <div className="subheader-item center">Gift Cards</div>
+
+                <div className="subheader-item center">Username's Amazon.com</div>
+
+                <div className="subheader-item center">Customer Service</div>
+
+                <div className="subheader-item center">Registry</div>
+
+                <div className="subheader-item center">
+                    Browsing History
+                    <BsCaretDownFill className="subheader-arrow" />
+                </div>
+                <div className="subheader-item center">Sell</div>
+            </div>
+        </div>
+    );
+};
+
+export default SubHeader;
--- src/components/Store/Store.js
+++ src/components/Store/Store.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import PageWrapper from "../UI/PageWrapper";
 import Header from "../Header-Footer/Header";
+import SubHeader from "../Header-Footer/SubHeader";
 
 const Store = () => {
     return (
         <PageWrapper>
             <Header />
+            <SubHeader />
         </PageWrapper>
     );
 };
