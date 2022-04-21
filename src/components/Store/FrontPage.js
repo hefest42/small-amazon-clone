b3b525cabe7af85db3c916b414da2753e478c9c3
Nemanja Vidovic <hefest93@yahoo.com>
2022-04-21 17:14:34 +0200

    working on banner

--- src/components/Store/FrontPage.js
+++ src/components/Store/FrontPage.js
@@ -3,9 +3,9 @@ import React from "react";
 import Product from "./Product";
 
 const bannerLinks = [
-    "https://m.media-amazon.com/images/I/61jovjd+f9L._SX3740_.jpg",
     "https://m.media-amazon.com/images/I/61DUO0NqyyL._SX3740_.jpg",
     "https://m.media-amazon.com/images/I/61TD5JLGhIL._SX3740_.jpg",
+    "https://m.media-amazon.com/images/I/61jovjd+f9L._SX3740_.jpg",
 ];
 
 const FrontPage = () => {
@@ -13,7 +13,14 @@ const FrontPage = () => {
         <div className="front center-column">
             <div className="front-banner center">
                 {bannerLinks.map((link, i) => (
-                    <img key={i} src={link} alt="banner" />
+                    <img
+                        key={i}
+                        src={link}
+                        alt="banner"
+                        style={{
+                            transfrom: `translateX(${i * 100}%)`,
+                        }}
+                    />
                 ))}
             </div>
 
