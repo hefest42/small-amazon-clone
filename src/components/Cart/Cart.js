c1b567404c04fbcbad9cc40fe152c649b38e72a0
Nemanja Vidovic <hefest93@yahoo.com>
2022-05-02 12:56:56 +0200

    added a message when user has no items in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
 import CartItem from "./CartItem";
 
 const Cart = ({ cartItems, removeItemFromCart }) => {
@@ -10,22 +12,33 @@ const Cart = ({ cartItems, removeItemFromCart }) => {
 
     return (
         <div className="cart">
-            <div className="cart-left">
-                {cartItems.map((item) => (
-                    <CartItem item={item} removeItem={removeItemFromCart} />
-                ))}
-            </div>
-
-            <div className="cart-right center-column">
-                <div className="cart-right__inner">
-                    <div className="cart-right__title">Cart</div>
-                    <div className="cart-right__total space-between">
-                        <div>Total:</div>
-                        <div>${totalPrice}</div>
-                    </div>
-                    <button className="button-orange">Buy</button>
+            {cartItems.length === 0 && (
+                <div className="cart-empty center-column">
+                    <p>You have no items in your Cart</p>
+                    <Link to="/store">Browse our selection</Link>
                 </div>
-            </div>
+            )}
+
+            {cartItems.length > 0 && (
+                <>
+                    <div className="cart-left">
+                        {cartItems.map((item) => (
+                            <CartItem item={item} removeItem={removeItemFromCart} />
+                        ))}
+                    </div>
+
+                    <div className="cart-right center-column">
+                        <div className="cart-right__inner">
+                            <div className="cart-right__title">Cart</div>
+                            <div className="cart-right__total space-between">
+                                <div>Total:</div>
+                                <div>${totalPrice}</div>
+                            </div>
+                            <button className="button-orange">Buy</button>
+                        </div>
+                    </div>
+                </>
+            )}
         </div>
     );
 };
