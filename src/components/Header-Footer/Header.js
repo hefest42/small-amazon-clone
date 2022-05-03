ff279236b6a30043769f91221a91d8145b417697
Nemanja Vidovic <hefest93@yahoo.com>
2022-05-03 13:09:23 +0200

    log out added

--- src/components/Header-Footer/Header.js
+++ src/components/Header-Footer/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -7,6 +7,8 @@ import { BsCart, BsCaretDownFill } from "react-icons/bs";
 import { AiFillCaretDown, AiOutlineSearch } from "react-icons/ai";
 
 const Header = ({ cartItems, showCartHandler, changeBackdrop, account }) => {
+    const [showSettings, setShowSettings] = useState(false);
+
     const mouseEnterCartHandler = () => {
         if (cartItems.length > 0) {
             showCartHandler(true);
@@ -56,18 +58,31 @@ const Header = ({ cartItems, showCartHandler, changeBackdrop, account }) => {
                 </div>
 
                 {account && (
-                    <div className="header-split header-part">
-                        <div className="header-split__right">
-                            <span className="header-split__top">{`Hello, ${account.name}`}</span>
-                            <span className="header-split__down">
-                                {`Account & Lists`}
-                                <BsCaretDownFill className="subheader-arrow" />
-                            </span>
+                    <>
+                        <div className="header-split header-part" onMouseEnter={() => setShowSettings(true)}>
+                            <div className="header-split__right">
+                                <span className="header-split__top">{`Hello, ${account.name}`}</span>
+                                <span className="header-split__down">
+                                    {`Account & Lists`}
+                                    <BsCaretDownFill className="subheader-arrow" />
+                                </span>
+                            </div>
                         </div>
-                    </div>
+
+                        {showSettings && (
+                            <div className="header-settings center-column" onMouseLeave={() => setShowSettings(false)}>
+                                <p>Account Settings</p>
+                                <p>Log Out</p>
+                            </div>
+                        )}
+                    </>
                 )}
 
-                {!account && <div className="header-split header-part">Log In</div>}
+                {!account && (
+                    <Link to="/log-in" className="header-split header-part">
+                        <div>Log In</div>
+                    </Link>
+                )}
 
                 <div className="header-split header-part">
                     <div className="header-split__right">
